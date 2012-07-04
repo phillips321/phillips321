ffb5f7165c977158760f5f0cabc19f40f228e68f
phillips321@gmail.com <phillips321@gmail.com@0285580d-d798-6be8-d631-bac73096fb64>
2012-07-04 13:21:06 +0000

    #	v0.2 - added folder search to portfinder.sh

--- trunk/portfinder.sh
+++ trunk/portfinder.sh
@@ -4,14 +4,15 @@
 # License:    CC BY-SA 3.0
 # Use:        Looks through *.gnmap output for port and outputs IPs
 # Released:   www.phillips321.co.uk
-  version=0.1
+  version=0.2
 # ToDo:
 #	nothing yet
 # ChangeLog:
+#	v0.2 - added folder search
 #	v0.1 - First write
 #___________________________________________________________
 if [ -z ${1} ] ; then echo -e "Usage: $0 [port] \n Example: $0 80" ; exit 1 ; fi
-cat *.gnmap | grep "\ ${1}/open" | cut -d" " -f2 | sort | uniq
+cat */*.gnmap | grep "\ ${1}/open" | cut -d" " -f2 | sort | uniq
 
 #for i in `cat open_ports.txt | grep open | grep -v http | grep -v "OS\ CPE" | grep -v fingerprint | grep -v "|" | cut -f1 -d"/" | sort | uniq | sort -n`
 #do
