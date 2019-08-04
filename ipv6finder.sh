88d49117fd72a0b4979e27c90c1b78c658f29b27
Nico Isenbeck <nico.isenbeck@web.de>
2019-08-04 11:03:32 +0200

    ipv6finder.sh: Clean up, remove MacOS support, remove "ifconfig" dependency,

--- ipv6finder.sh
+++ ipv6finder.sh
@@ -17,64 +17,45 @@
 
 f_main(){
     #Ping broadcast address for local neighbours and store as LinkLocalNeighbours
-    echo -n "[+]Pinging (ff02::1) broadcast for nodes on link local"
-    LinkLocalNeighbours=`ping6 -c 3 -I ${interface} ff02::1 | grep icmp_seq | cut -d" " -f4 | cut -d"," -f 1 | sort -u` ; echo -n "."
+    echo -n "[+]Pinging (ff02::1) broadcast for nodes on link local. "
+    LinkLocalNeighbours=$(ping6 -c 3 -I ${interface} ff02::1 | grep icmp_seq | cut -d" " -f4 | cut -d"," -f 1 | sort -u)
     echo "Done"
 
     #Ping broadcast address for router neighbours and store as RouterLocalNeighbours
-    echo -n "[+]Pinging (ff02::2) broadcast for routers"
-    RouterLocalNeighbours=`ping6 -c 3 -I ${interface} ff02::2 | grep icmp_seq | cut -d" " -f4 | cut -d"," -f 1 | sort -u` ; echo -n "."
+    echo -n "[+]Pinging (ff02::2) broadcast for routers. "
+    RouterLocalNeighbours=$(ping6 -c 3 -I ${interface} ff02::2 | grep icmp_seq | cut -d" " -f4 | cut -d"," -f 1 | sort -u)
     echo "Done"
 
     #Ping broadcast address for global neighbours and store as GlobalNeighbours
-    echo -n "[+]Pinging (ff02::1) broadcast for nodes on Global Interface" ;
-        if [ "$(uname)" == "Darwin" ]; then #must be OS X
-            IPV6Address=`ifconfig ${interface} | grep inet6 | grep -v fe80 | grep -v temporary | grep -v "::1" | awk {'print $2'}`
-            if [ ! -z ${IPV6Address} ]; then
-                GlobalNeighbours=`ping6 -c 3 -I ${interface} -S ${IPV6Address} ff02::1 | grep icmp_seq | cut -d" " -f4 | cut -d"," -f 1 | sort -u` ; echo -n "."
-                { for i in ${GlobalNeighbours} ; do ping6 -c 1 -I ${interface} $i ; done } &> /dev/null
-            else
-                IPV6Address=""; GlobalNeighbours=""
-            fi
-        else #must be Linux/Cywin?
-            IPV6Address=`ip addr | grep inet6 | grep -v "::1" | grep -v fe80 | grep -v "temporary" | awk {'print $2'} | cut -d"/" -f1`
-            if [ ! -z ${IPV6Address} ]; then
-                GlobalNeighbours=`ping6 -c 3 -I ${IPV6Address} ff02::1 | grep icmp_seq | cut -d" " -f4 | cut -d"," -f 1 | sort -u | rev | cut -c 2- | rev` ; echo -n "."
-                { for i in ${GlobalNeighbours} ; do ping6 -c 1 -I ${interface} $i ; done } &> /dev/null
-            else
-                IPV6Address=""; GlobalNeighbours=""
-            fi
-        fi
+    echo -n "[+]Pinging (ff02::1) broadcast for nodes on Global Interface. "
+    IPV6Address=$(ip -6 addr | grep inet6 | grep -v "::1" | grep -v fe80 | grep -v "temporary" | awk {'print $2'} | cut -d"/" -f1)
+    if [ ! -z ${IPV6Address} ]; then
+        GlobalNeighbours=$(ping6 -c 3 -I ${IPV6Address} ff02::1 | grep icmp_seq | cut -d" " -f4 | cut -d"," -f 1 | sort -u | rev | cut -c 2- | rev)
+        { for i in ${GlobalNeighbours} ; do ping6 -c 1 -I ${interface} $i ; done } &> /dev/null
+    else
+        IPV6Address=""; GlobalNeighbours=""
+    fi
     echo "Done"
 
-    echo -n "[+]ArpScanning local IPv4"
-    ArpScan=`arp-scan -l -I ${interface} | grep -v packets | grep -v DUP | grep -v ${interface} | grep -v Starting | grep -v Ending | cut -f1,2`
-    echo ".Done"
+    echo -n "[+]ArpScanning local IPv4. "
+    ArpScan=$(arp-scan -l -I ${interface} | grep -v packets | grep -v DUP | grep -v ${interface} | grep -v Starting | grep -v Ending | cut -f1,2)
+    echo "Done"
 
     echo "--------------------------------|------------------------------------------|--------------------|--------------------|-------------"
     printf "%31s %1s %40s %1s %18s %1s %18s %1s %12s\n" "IPV6 Link Local" "|" "IPV6 Global" "|" "MAC Address" "|" "IPV4Address" "|" "Info"
     echo "--------------------------------|------------------------------------------|--------------------|--------------------|-------------"
     for IPV6LL in ${LinkLocalNeighbours}; do
         #Get LinkLocal MAC from NDP table
-        if [ "$(uname)" == "Darwin" ]; then #must be OS X
-            ShortMAC=`ndp -nl ${IPV6LL} | grep ${IPV6LL} | awk '{print $2}'`
-            LongMAC=`echo ${ShortMAC} | awk -F: '{printf("%02s:%02s:%02s:%02s:%02s:%02s",$1,$2,$3,$4,$5,$6)}' ; echo ""`
-        else #must be Linux/Cywin?
-            ShortMAC=`ip -6 neigh show $(echo ${IPV6LL} | head -n1 | cut -d"%" -f1) | awk {'print $5'} | sed 's/0\([0-9A-Fa-f]\)/\1/g'`
-            LongMAC=`ip -6 neigh show $(echo ${IPV6LL} | head -n1 | cut -d"%" -f1) | awk {'print $5'}`
-            if [ -z ${ShortMAC} ] ; then ShortMAC=`cat /sys/class/net/${interface}/address | sed 's/0\([0-9A-Fa-f]\)/\1/g'`; fi
-            if [ -z ${LongMAC} ] ; then LongMAC=`cat /sys/class/net/${interface}/address`; fi
-        fi
+        ShortMAC=$(ip -6 neigh show $(echo ${IPV6LL} | head -n1 | cut -d"%" -f1) | awk {'print $5'} | sed 's/0\([0-9A-Fa-f]\)/\1/g')
+        LongMAC=$(ip -6 neigh show $(echo ${IPV6LL} | head -n1 | cut -d"%" -f1) | awk {'print $5'})
+        if [ -z ${ShortMAC} ] ; then ShortMAC=$(cat /sys/class/net/${interface}/address | sed 's/0\([0-9A-Fa-f]\)/\1/g'); fi
+        if [ -z ${LongMAC} ] ; then LongMAC=$(cat /sys/class/net/${interface}/address); fi
 
         #Use MAC to pair up with IPv4 address and global IPv6
         if [ ! -z ${ShortMAC} ]; then
-            IPV4Address=`echo "${ArpScan}" | grep "${LongMAC}" | head -n1 | cut -f1`
+            IPV4Address=$(echo "${ArpScan}" | grep "${LongMAC}" | head -n1 | cut -f1)
             IPV6G=""
-            if [ "$(uname)" == "Darwin" ]; then #must be OS X
-                IPV6G=`ndp -anl | grep ${ShortMAC} | grep -v fe80 | awk {'print $1'} | tail -n 1`
-            else #must be Linux/Cywin?
-                IPV6G=`ip -6 neigh show | grep "${LongMAC}" | grep -v fe80 | awk {'print $1'} | head -n 1`
-            fi
+            IPV6G=$(ip -6 neigh show | grep "${LongMAC}" | grep -v fe80 | awk {'print $1'} | head -n 1)
             if [ -z ${IPV6G} ]; then IPV6G="NotFound" ; fi
         fi
 
@@ -82,12 +63,7 @@ f_main(){
 
         #IPv4 not found so might be you or not in subnet?
         if [ -z ${IPV4Address} ]; then #Unable to find IPv4 so possibly you
-            if [ "$(uname)" == "Darwin" ]; then #must be OS X
-                #IPV4Address=`arp -anl | grep ${ShortMAC} | awk '{print $1}'` #commented out on 10/08/2017 due to bug
-                IPV4Address=`ifconfig ${interface} | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`
-            else #must be Linux/Cywin?
-                IPV4Address=`ifconfig ${interface} | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`
-            fi
+            IPV4Address=$(ip -4 addr show ${interface} | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
             Info="You?"
         else #IPv4 found so now decididng if router or not
             if (echo "$RouterLocalNeighbours" | grep -q ${IPV6LL}) ; then Info="Router"; else Info="Node" ; fi
